5c3df36a79e9127155ea0be53449f711c642ecc7
emisua <10.khima@gmail.com>
2022-02-26 01:03:21 +0100

    Refactorizado de componente title con props

--- componentes/Title.jsx
+++ componentes/Title.jsx
@@ -1,7 +1,12 @@
 import "../src/css/Title.css";
 
-const Title = ({ title }) => {
-  return <h3 className="title">{title}</h3>;
+const Title = ({ children, title, size, className }) => {
+  const Tag = `h${size}`;
+  if (title != undefined) {
+    return <Tag className={`title ${className}`}>{title}</Tag>;
+  } else {
+    return <Tag className={`title ${className}`}>{children}</Tag>;
+  }
 };
 
 export default Title;
