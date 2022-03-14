de1e6e5a16e75c2e5bee765b2ffd7fbdfb78cfbf
emisua <10.khima@gmail.com>
2022-03-14 13:01:53 +0100

    Cambiadas imágenes por componente Figure

--- componentes/paginas/PaginaSobreMi.jsx
+++ componentes/paginas/PaginaSobreMi.jsx
@@ -1,5 +1,6 @@
 import Title from '../Title'
 import Header from '../Header'
+import Figure from '../Figure'
 import '../../src/css/Sobremi.css'
 
 const SobreMi = () => {
@@ -19,45 +20,62 @@ const SobreMi = () => {
             <p>
               Soy un <strong>apasionado de la tecnología.</strong> Desde que descubrí el diseño y desarrollo web, no he parado de indagar en ello.
             </p>
-            <p>Actualmente estoy centrado en <strong className='featured'>aprender, mejorar, y especializarme en <i>JavaScript</i></strong>. Esto lo acompaño con el framework <strong><i className='color-react'>React⚛️</i></strong>, con el que está hecho este portfolio.</p> En un futuro me gustaría aprender otros frameworks como <i>Vue</i> o <i>Angular</i>.
+            <p>Actualmente estoy centrado en <strong className='featured'>aprender, mejorar, y especializarme en <i>JavaScript</i></strong>. Esto lo acompaño con el framework <strong><img width='12' src='../../src/img/logos/react.jpg' /><i className='color-react'>React</i></strong>, con el que está hecho este portfolio.</p> En un futuro me gustaría aprender otros frameworks como <i>Vue</i> o <i>Angular</i>. Soy autodidacta y aprendo algo nuevo cada día.
             <p />
           </div>
           <div className='tecnologias'>
             <Title size='3' title='Tecnologías que utilizo' />
             <ul className='grid-tecnologias'>
-              <li><img src='../../src/img/logos/html.jpg' />
-                <span>HTML</span>
+              <li>
+                <Figure
+                  src='../../src/img/logos/html.jpg'
+                  label='HTML'
+                />
               </li>
-              <li><img src='../../src/img/logos/css.jpg' />
-                <span>CSS</span>
+              <li><Figure
+                src='../../src/img/logos/css.jpg'
+                label='CSS'
+                  />
               </li>
-              <li><img src='../../src/img/logos/js.jpg' />
-                <span>Javascript</span>
+              <li><Figure
+                src='../../src/img/logos/js.jpg'
+                label='JS'
+                  />
               </li>
-              <li><img src='../../src/img/logos/react.jpg' />
-                <span>React JS</span>
+              <li><Figure
+                src='../../src/img/logos/react.jpg'
+                label='React'
+                  />
               </li>
             </ul>
           </div>
           <div className='cms'>
             <Title size='3' title='CMSs que utilizo' />
             <ul className='grid-tecnologias'>
-              <li><img src='../../src/img/logos/wordpress.jpg' />
-                <span>Wordpress</span>
+              <li><Figure
+                src='../../src/img/logos/wordpress.jpg'
+                label='Wordpress'
+                  />
               </li>
-              <li><img src='../../src/img/logos/orchard.jpg' />
-                <span>Orchard</span>
+              <li><Figure
+                src='../../src/img/logos/orchard.jpg'
+                label='Orchard'
+                  />
               </li>
-              <li><img src='../../src/img/logos/ghost.jpg' />
-                <span>Ghost</span>
+              <li><Figure
+                src='../../src/img/logos/ghost.jpg'
+                label='Ghost'
+                  />
               </li>
             </ul>
           </div>
           <div className='ecommerce'>
             <Title size='3' title='eCommerce que utilizo' />
             <ul className='grid-tecnologias'>
-              <li><img src='../../src/img/logos/prestashop.jpg' />
-                <span>Prestashop</span>
+              <li><Figure
+                src='../../src/img/logos/prestashop.jpg'
+                label='Prestashop'
+                  />
               </li>
             </ul>
           </div>
