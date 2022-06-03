f622d866c59b2666ac0374f5a4953e4107b4b100
emisua <10.khima@gmail.com>
2022-06-03 20:06:43 +0200

    Añado cursor al icono del modo oscuro

--- componentes/Navbar.jsx
+++ componentes/Navbar.jsx
@@ -66,7 +66,7 @@ const Navbar = ({ isMobile }) => {
         </div>
       </Link>
       <a
-        className='w-4 mr-8 md:hidden'
+        className='w-4 mr-8 md:hidden cursor-pointer'
         onClick={() => {
           setDarkMode(!darkMode)
           document.querySelector('html').classList.toggle('dark')
@@ -91,7 +91,7 @@ const Navbar = ({ isMobile }) => {
               <path d='M4 20L5 19' stroke='white' strokeLinecap='round' strokeLinejoin='round' />
               <path d='M4 4L5 5' stroke='white' strokeLinecap='round' strokeLinejoin='round' />
               <path d='M1 12L2 12' stroke='white' strokeLinecap='round' strokeLinejoin='round' />
-              </>
+            </>
             : <path // LUNA
                 d='M3 11.5066C3 16.7497 7.25034 21 12.4934 21C16.2209 21 19.4466 18.8518 21 15.7259C12.4934 15.7259 8.27411 11.5066 8.27411 3C5.14821 4.55344 3 7.77915 3 11.5066Z'
                 stroke='black'
@@ -161,7 +161,7 @@ const Navbar = ({ isMobile }) => {
                 <path d='M4 20L5 19' stroke='white' strokeLinecap='round' strokeLinejoin='round' />
                 <path d='M4 4L5 5' stroke='white' strokeLinecap='round' strokeLinejoin='round' />
                 <path d='M1 12L2 12' stroke='white' strokeLinecap='round' strokeLinejoin='round' />
-              </>
+                </>
               : <path // LUNA
                   d='M3 11.5066C3 16.7497 7.25034 21 12.4934 21C16.2209 21 19.4466 18.8518 21 15.7259C12.4934 15.7259 8.27411 11.5066 8.27411 3C5.14821 4.55344 3 7.77915 3 11.5066Z'
                   stroke='black'
